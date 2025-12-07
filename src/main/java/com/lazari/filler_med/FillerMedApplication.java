f82d6d0bae1cfa779766d5bca81e5502c2267209
LazariaInk <lorienlored@gmail.com>
2025-12-07 16:28:03 +0300

    add classes in git

---
+++ src/main/java/com/lazari/filler_med/FillerMedApplication.java
@@ -0,0 +1,11 @@
+package com.lazari.filler_med;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FillerMedApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FillerMedApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lazari/filler_med/FillerMedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazari.filler_med;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FillerMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
